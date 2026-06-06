7632f9480442cf9449467aa0c41e3d2a747ae952
Deeva-Civia <s22210557@student.unklab.ac.id>
2026-06-06 16:25:10 +0800

    update navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -8,8 +8,8 @@ const navItems = [
   { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
-  { name: "Projects", href: "#projects" },
   { name: "Experience", href: "#experience" },
+  { name: "Projects", href: "#projects" },
   { name: "Contact", href: "#contact" },
 ]
 
